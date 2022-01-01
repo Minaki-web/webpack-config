3a964280f2bf14c85f1166b57aade54cc89291f4
Minaki-web <mine.mnk0627@gmail.com>
2022-01-01 12:00:36 +0900

    add: scss, post-css, hot-reloading

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './scss/style.scss'
+
 const pizza = {
   base: 'crispy',
   cheese: 100
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path')
 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries')
+
 let mode = process.env.NODE_ENV === 'production' ? 'production' : 'development'
 
 module.exports = {
@@ -11,6 +14,30 @@ module.exports = {
       directory: path.join(__dirname, 'dist')
     },
     compress: true,
+    hot: true,
     port: 9000
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  ['postcss-sort-media-queries', { sort: 'desktop-first' }]
+                ]
+              }
+            }
+          },
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [new MiniCssExtractPlugin()]
 }
