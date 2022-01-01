f2c2a4e0b73f2f5abdd3e62a358e7ca8e83ce937
Minaki-web <mine.mnk0627@gmail.com>
2022-01-01 23:43:20 +0900

    Change folder structure, reload settings

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,16 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { glob } = require('glob')
 
 let mode = process.env.NODE_ENV === 'production' ? 'production' : 'development'
 
-module.exports = {
+const htmlFiles = glob.sync('pages/*.html', {
+  cwd: './src',
+  ignore: 'pages/index.html'
+})
+
+const buildConfig = {
   mode: mode,
   devtool: 'source-map',
   target: ['web', 'es5'],
@@ -18,16 +24,19 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js'
+    filename: 'js/[name].bundle.js'
   },
 
   devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist')
-    },
+    static: [
+      {
+        directory: path.join(__dirname, 'dist')
+      }
+    ],
     compress: true,
-    hot: true,
-    port: 9000
+    // hot: true,
+    port: 9000,
+    watchFiles: ['src/**/*.php', 'src/**/*.html']
   },
 
   module: {
@@ -62,18 +71,15 @@ module.exports = {
 
   plugins: [
     new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    }),
     new HtmlWebpackPlugin({
       inject: 'body',
       chunks: ['main'],
       template: './src/pages/index.html'
     }),
-    new HtmlWebpackPlugin({
-      inject: 'body',
-      filename: 'about/index.html',
-      chunks: ['about'],
-      template: './src/pages/about.html'
-    }),
+
     new CopyWebpackPlugin({
       patterns: [{ from: 'src/images', to: 'images' }]
     })
@@ -91,3 +97,16 @@ module.exports = {
     }
   }
 }
+
+htmlFiles.forEach((file) => {
+  buildConfig.plugins.push(
+    new HtmlWebpackPlugin({
+      inject: 'body',
+      filename: `${file.split('/')[1].split('.')[0]}/index.html`,
+      chunks: [file.split('/')[1].split('.')[0]],
+      template: `./src/${file}`
+    })
+  )
+})
+
+module.exports = buildConfig
