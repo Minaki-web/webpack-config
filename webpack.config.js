17b11d62850fed80864de9aead0bb39d18c9f3f5
Minaki-web <mine.mnk0627@gmail.com>
2022-01-01 13:19:05 +0900

    add images, html management

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,23 @@
 const path = require('path')
 
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 let mode = process.env.NODE_ENV === 'production' ? 'production' : 'development'
 
 module.exports = {
   mode: mode,
   devtool: 'source-map',
+
+  entry: {
+    main: './src/js/index.js',
+    about: './src/js/about.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist')
+  },
 
   devServer: {
     static: {
@@ -17,13 +27,23 @@ module.exports = {
     hot: true,
     port: 9000
   },
+
   module: {
     rules: [
       {
         test: /\.(s[ac]|c)ss$/i,
         use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { publicPath: '' }
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+              importLoaders: 2
+            }
+          },
           {
             loader: 'postcss-loader',
             options: {
@@ -39,5 +59,27 @@ module.exports = {
       }
     ]
   },
-  plugins: [new MiniCssExtractPlugin()]
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      inject: false,
+      chunks: ['main'],
+      template: './src/pages/index.html'
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      filename: 'about/index.html',
+      chunks: ['about'],
+      template: './src/pages/about.html'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: 'src/images', to: 'images' }]
+    })
+  ],
+
+  resolve: {
+    extensions: ['.ts', '.js']
+  }
 }
